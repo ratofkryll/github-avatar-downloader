5a977a82f7c1b0279a0a95dad102e265da0e2c50
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-04 03:58:48 +0000

    output user avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -5,18 +5,25 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors (repoOwner, repoName, cb) {
   var options = {
-    url: `https://api.github.com/repos/' ${repoOwner}/${repoName}/contributors`,
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'request',
-      'Authorization':
+      'Authorization': secrets.GITHUB_TOKEN
     }
   };
-  request (url, function (err, res, body) {
-    cb(err, body);
+  var parsedBody = [];
+  request (options, function (err, res, body) {
+    parsedBody = JSON.parse(body);
+    // console.log(parsedBody[0].login + '\'s Avatar URL: ' + parsedBody[0].avatar_url)
+    cb(err, parsedBody);
   });
 }
 
+
 getRepoContributors ('jquery', 'jquery', function (err, result) {
   console.log('Errors: ', err);
-  console.log('Results: ', result);
+  result.forEach(function (user) {
+    console.log('Results: ', user.avatar_url);
+  });
+  // console.log('Results: ', result[0].login + '\'s Avatar URL: ' + result[0].avatar_url);
 });
