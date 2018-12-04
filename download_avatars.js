c3710fc69dc1103f496f8ab9d72834024826d61f
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-04 05:51:31 +0000

    added detect/create file path & downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,12 @@
 var request = require('request');
+var fs = require('fs');
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+if (!fs.existsSync('./avatars')) {
+  fs.mkdirSync('./avatars');
+}
 
 function getRepoContributors (repoOwner, repoName, cb) {
   var options = {
@@ -20,13 +25,26 @@ function getRepoContributors (repoOwner, repoName, cb) {
 }
 
 function downloadImageByURL (url, filepath) {
-  
+  request.get(url)
+  .on('error', function (err) {
+    console.log('Error: ', err.statusCode)
+  })
+  .on('response', function (response) {
+    console.log(`
+      Response Status Code: ${response.statusCode}\n
+      Response Status Message: ${response.statusMessage}\n
+      Response Headers: ${response.headers['content-type']}
+    `);
+  })
+  .pipe(fs.createWriteStream(filepath));
 }
 
 getRepoContributors ('jquery', 'jquery', function (err, result) {
   console.log('Errors: ', err);
-  result.forEach(function (user) {
-    console.log('Results: ', user.avatar_url);
-  });
+  // result.forEach(function (user) {
+  //   console.log('Results: ', user.avatar_url);
+  // });
   // console.log('Results: ', result[0].login + '\'s Avatar URL: ' + result[0].avatar_url);
 });
+
+downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
