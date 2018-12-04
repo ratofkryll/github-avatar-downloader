86c463a24ca9d32150d67128dae5f93fb331a173
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-04 06:30:10 +0000

    made owner and repo required arguments

--- download_avatars.js
+++ download_avatars.js
@@ -7,39 +7,44 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 var owner = process.argv[2];
 var repo = process.argv[3];
 
-// Checks if avatars directory exists. If not, creates it.
-if (!fs.existsSync('./avatars')) {
-  fs.mkdirSync('./avatars');
-};
-
-function getRepoContributors (repoOwner, repoName, cb) {
-  var options = {
-    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
-    }
+// If an owner or repo are not provided, does not attempt to download avatars.
+if (!owner || !repo) {
+  console.log('You must provide a valid Github owner and repository.')
+} else {
+  // Checks if avatars directory exists. If not, creates it.
+  if (!fs.existsSync('./avatars')) {
+    fs.mkdirSync('./avatars');
   };
-  var parsedBody = [];
-  request (options, function (err, res, body) {
-    parsedBody = JSON.parse(body);
-    cb(err, parsedBody);
+
+  function getRepoContributors (repoOwner, repoName, cb) {
+    var options = {
+      url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': secrets.GITHUB_TOKEN
+      }
+    };
+    var parsedBody = [];
+    request (options, function (err, res, body) {
+      parsedBody = JSON.parse(body);
+      cb(err, parsedBody);
+    });
+  };
+
+  function downloadImageByURL (url, filepath) {
+    request.get(url)
+    .on('error', function (err) {
+      console.log('Error: ', err.statusCode)
+    })
+    .pipe(fs.createWriteStream(filepath));
+    console.log('Download complete!')
+  };
+
+  getRepoContributors (owner, repo, function (err, result) {
+    console.log('Errors: ', err);
+    result.forEach(function (user) {
+      console.log(`Downloading ${user.login}'s avatar...`)
+      downloadImageByURL(user.avatar_url, `./avatars/${user.login}.jpg`)
+    });
   });
 };
-
-function downloadImageByURL (url, filepath) {
-  request.get(url)
-  .on('error', function (err) {
-    console.log('Error: ', err.statusCode)
-  })
-  .pipe(fs.createWriteStream(filepath));
-  console.log('Download complete!')
-};
-
-getRepoContributors (owner, repo, function (err, result) {
-  console.log('Errors: ', err);
-  result.forEach(function (user) {
-    console.log(`Downloading ${user.login}'s avatar...`)
-    downloadImageByURL(user.avatar_url, `./avatars/${user.login}.jpg`)
-  });
-});
