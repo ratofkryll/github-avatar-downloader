a20edb0176325f0c4a54161359a2848beae89c0e
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-04 03:03:54 +0000

    added getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors (repOwner, repName, cb) {
+
+}
+
+getRepoContributors ('jquery', 'jquery', function (err, result) {
+  console.log('Errors: ', err);
+  console.log('Results: ', result);
+});
